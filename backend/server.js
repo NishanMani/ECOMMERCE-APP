39f4d603138e8a50766ac047190ab2f0bd3ec197
GIRISH-VV <girishkumarvv19@gmail.com>
2026-02-27 18:29:31 +0530

    updated server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,10 @@ import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
 import connectDB from './src/config/db.js'
+import authRoutes from './src/routes/auth.route.js'
+import productRoutes from './src/routes/product.route.js'
+import orderRoutes from './src/routes/order.route.js'
+import userRoutes from './src/routes/user.route.js'
 
 dotenv.config()
 await connectDB()
@@ -9,6 +13,11 @@ await connectDB()
 const app = express()
 app.use(express.json())
 app.use(cors())
+
+app.use('/api/auth', authRoutes)
+app.use('/api/products', productRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/users', userRoutes)
 
 const PORT = process.env.PORT || 5000
 
